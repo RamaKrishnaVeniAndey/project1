deb09e334e272a8995f40b5e43f776dce1bb4581
RamaKrishnaVeniAndey <137883949+RamaKrishnaVeniAndey@users.noreply.github.com>
2023-07-19 10:37:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://images.unsplash.com/photo-1532007271951-c487760934ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxpZ2h0JTIwYnVsYnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://c4.wallpaperflare.com/wallpaper/744/430/547/sun-light-bulb-hd-wallpaper-preview.jpg";
+}
+function flower(){
+    document.getElementById("project").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXcEQyUg_XFGUVDKHuc6rEe929OTMgRXC4GFNC2B8Raw&s";
+}
+function fruit(){
+    document.getElementById("project").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWNkld2qvYi7vpo2UKfLBvCy4zGD9hcE9Sw6YsxC8D2g&s";
+}
+function vegetable(){
+    document.getElementById("project").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0MFggoKeRW5_sGHQlrw_rw0Teu7OKa0P2MyoFxRlCPg&s";
+}
